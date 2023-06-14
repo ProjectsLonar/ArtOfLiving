7689298f796bbbe4832a78683261a74f25316692
unknown <vaibhav.rupnar@lonartech.com>
2023-06-14 16:52:29 +0530

    changing portNo & add extends in main class

--- artofliving/src/main/java/com/lonar/artofliving/ArtoflivingApplication.java
+++ artofliving/src/main/java/com/lonar/artofliving/ArtoflivingApplication.java
@@ -5,7 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -23,7 +23,7 @@ import org.springframework.web.client.RestTemplate;
 @PropertySource({ "classpath:persistence.properties" })
 @EnableJpaRepositories(repositoryFactoryBeanClass = DataTablesRepositoryFactoryBean.class)
 @Configuration
-public class ArtoflivingApplication {
+public class ArtoflivingApplication extends SpringBootServletInitializer{
 	
 	@Autowired
 	private Environment env;
